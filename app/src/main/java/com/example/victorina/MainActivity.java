b7aa35e82feb6a49affdb4eef9b82172affe0efc
Takhirzhon <tashmatovtahir@gmail.com>
2022-12-28 21:37:51 +0600

    Button start is working now

--- app/src/main/java/com/example/victorina/MainActivity.java
+++ app/src/main/java/com/example/victorina/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.victorina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button start = (Button) findViewById(R.id.button);
+
+        start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                try {
+                    Intent intent = new Intent(MainActivity.this, GameLevel.class);
+                    startActivity(intent); finish();
+                } catch (Exception e) {
+
+                }
+            }
+        });
 
         Window bar = getWindow();
         bar.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
