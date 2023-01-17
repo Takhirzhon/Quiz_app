3b7a80ef6728974def529f0f60376e9d4ea0ca45
Takhirzhon <tashmatovtahir@gmail.com>
2023-01-18 01:53:24 +0600

    Added crop to img_left and img_bottom

--- app/src/main/java/com/example/victorina/Level1.java
+++ app/src/main/java/com/example/victorina/Level1.java
@@ -14,11 +14,22 @@ public class Level1 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.universal);
 
-        final ImageView imf_left = (ImageView)findViewById(R.id.img_left);
+        //код который скругляет углы ImageView for questions  - НАЧАЛО
+        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        img_left.setClipToOutline(true);
 
-        imf_left.setCropToPadding(true); //код который скругляет углы ImageView for questions
+        final ImageView img_left1 = (ImageView)findViewById(R.id.img_left1);
+        img_left1.setClipToOutline(true);
 
-        //развертывание игры на вес экарн
+        final ImageView img_bottom = (ImageView)findViewById(R.id.img_bottom);
+        img_bottom.setClipToOutline(true);
+
+        final ImageView img_bottom1 = (ImageView)findViewById(R.id.img_bottom1);
+        img_bottom1.setClipToOutline(true);
+        //код который скругляет углы ImageView for questions  - КОНЕЦ
+
+
+        //развертывание игры на весь экарн
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         // конец развертывания
