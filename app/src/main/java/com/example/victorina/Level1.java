f3158d92ba0d7537b467dd4a720de527d2eba1f3
Takhirzhon <tashmatovtahir@gmail.com>
2023-02-02 16:03:37 +0600

    Back Press working

--- app/src/main/java/com/example/victorina/Level1.java
+++ app/src/main/java/com/example/victorina/Level1.java
@@ -2,9 +2,12 @@ package com.example.victorina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class Level1 extends AppCompatActivity {
@@ -15,16 +18,16 @@ public class Level1 extends AppCompatActivity {
         setContentView(R.layout.universal);
 
         //код который скругляет углы ImageView for questions  - НАЧАЛО
-        final ImageView img_left = (ImageView)findViewById(R.id.img_left);
+        final ImageView img_left = (ImageView) findViewById(R.id.img_left);
         img_left.setClipToOutline(true);
 
-        final ImageView img_left1 = (ImageView)findViewById(R.id.img_left1);
+        final ImageView img_left1 = (ImageView) findViewById(R.id.img_left1);
         img_left1.setClipToOutline(true);
 
-        final ImageView img_bottom = (ImageView)findViewById(R.id.img_bottom);
+        final ImageView img_bottom = (ImageView) findViewById(R.id.img_bottom);
         img_bottom.setClipToOutline(true);
 
-        final ImageView img_bottom1 = (ImageView)findViewById(R.id.img_bottom1);
+        final ImageView img_bottom1 = (ImageView) findViewById(R.id.img_bottom1);
         img_bottom1.setClipToOutline(true);
         //код который скругляет углы ImageView for questions  - КОНЕЦ
 
@@ -33,5 +36,43 @@ public class Level1 extends AppCompatActivity {
         Window w = getWindow();
         w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
         // конец развертывания
+
+        //кнопка Назад Начало
+        Button btn_back = (Button) findViewById(R.id.buttonback);
+        btn_back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Обрабатываем нажатие кнопки назад НАЧАЛО
+                try {
+                    //Вернуться назад к выбору уровня НАЧАЛО
+                    Intent intent = new Intent(Level1.this, GameLevel.class);//Создал намерени для перехода
+                    startActivity(intent);//Старт Намерения
+                    finish();
+                    //Вернуться назад к выбору уровня КОНЕЦ
+
+                } catch (Exception e) {
+
+                }
+                //Обрабатываем нажатие кнопки назад КОНЕЦ
+            }
+        });
+        //Кнопка Назад Конец
     }
+    //Системная кнопка Назад Начало
+    @Override
+    public void onBackPressed() {
+        //Обрабатываем нажатие кнопки назад НАЧАЛО
+        try {
+            //Вернуться назад к выбору уровня НАЧАЛО
+            Intent intent = new Intent(Level1.this, GameLevel.class);//Создал намерени для перехода
+            startActivity(intent);//Старт Намерения
+            finish();
+            //Вернуться назад к выбору уровня КОНЕЦ
+
+        } catch (Exception e) {
+
+        }
+        //Обрабатываем нажатие кнопки назад КОНЕЦ
+    }
+    //Системная кнопка Назад КОНЕЦ
 }
